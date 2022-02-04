521e5a12048cded103fe5978dd9e5d0f07e8e999
Paula Ardelean <paula.ardelean.b@gmail.com>
2022-02-04 23:19:06 +0200

    main frontend work

--- frontend/animal-shelter-mainfe/src/app/app-routing.module.ts
+++ frontend/animal-shelter-mainfe/src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppLayoutComponent } from './layouts/app-layout/app-layout.component';
+import { EmptyLayoutComponent } from './layouts/empty/empty-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'home',
+    pathMatch:'full'
+  },
+  {
+    path:'authentication',
+    component: EmptyLayoutComponent,
+    children: [
+      {
+        path:'',
+        loadChildren: () => import('./modules/authentication/authentication.module').then(a => a.AuthenticationModule)
+      }
+    ]
+  },
+  {
+    path:'home',
+    component: AppLayoutComponent,
+    children: [
+      {
+        path:'',
+        loadChildren: () => import('./modules/home/home.module').then(h => h.HomeModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
